8daf391b5fb282b0efebbd2f8213a9bbd96d4571
Gali Bhaskar <luciferbhaskar@gmail.com>
2023-03-19 12:05:50 -0700

    Updated the implementation of test_return.py

--- src/tests/test_return.py
+++ src/tests/test_return.py
@@ -1,52 +1,119 @@
 import unittest
 import subprocess
 from gradescope_utils.autograder_utils.decorators import weight, tags
-from utility import get_code_file_name
+from utility import get_code_file_name, get_function_info, get_test_cases, define_function_call
 import ctypes
 import os
 
-code_file_name = get_code_file_name()
-
 
 class TestReturn(unittest.TestCase):
-    test_cases = [
-        {"input": [1, 2, 3], "expected_output": 6, "weight": 2},
-        {"input": [-1, -2, -3], "expected_output": -6, "weight": 2},
-        {"input": [0, 0, 0, 0, 0, 0], "expected_output": 0, "weight": 2},
-        {"input": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-            "expected_output": 55, "weight": 2},
-        {"input": [-10, 5, 3, -2, 0, 8, -7],
-            "expected_output": -3, "weight": 2},
-        {"input": [], "expected_output": 0, "weight": 2}
-    ]
+    code_file_name = get_code_file_name()
 
-    lib = None
+    test_cases = get_test_cases()
 
-    def compile_code(self):
-        cmd = f"gcc -shared -o test.so {code_file_name}"
+    function_info = get_function_info()
+
+    def wrap_function(self, funcname, restype, argtypes):
+        func = self.lib.__getattr__(funcname)
+
+        func.restype = self.get_arg_type(restype)
+
+        func.argtypes = [self.get_arg_type(arg) for arg in argtypes]
+
+        return func
+
+    def is_syntax_correct(self):
+        cmd = f"gcc -c {self.code_file_name}"
+
         result = subprocess.run(
             cmd, shell=True, capture_output=True, text=True)
-        
+
+        return result.returncode == 0
+
+    def generate_shared_code(self):
+        cmd = f"gcc -shared -o test.so {self.code_file_name}"
+
+        result = subprocess.run(
+            cmd, shell=True, capture_output=True, text=True)
+
+    def get_arg_type(self, arg):
+        if arg == "<class 'ctypes.c_void_p'>":
+            return ctypes.c_void_p
+
+        elif arg == "<class 'ctypes._Pointer'> <class 'ctypes.c_int'>":
+            return ctypes.POINTER(ctypes.c_int)
+
+        elif arg == "<class 'ctypes.c_int'>":
+            return ctypes.c_int
+
+        elif arg == "<class 'list'>":
+            return ctypes.POINTER(ctypes.c_int)
+
+        else:
+            return ctypes.c_int
+
+    def get_arg_value(self, arg_type, arg):
+        # print(arg_type," ", arg)
+        if arg_type == ctypes.POINTER(ctypes.c_int):
+            return ((ctypes.c_int)*len(arg))(*arg)
+
+        else:
+            return arg
 
     def setUp(self):
-        self.compile_code()
+        # Define the function signature
+        self.FuncType = ctypes.CFUNCTYPE(
+            ctypes.c_void_p, *([ctypes.c_void_p] * ctypes.sizeof(ctypes.c_void_p)))
 
-        self.lib = ctypes.CDLL(os.path.abspath("./test.so"))
+        self.lib = None
 
-        if self.lib:
-            self.lib.calculateTotalInArray.argtypes = [
-                ctypes.POINTER(ctypes.c_int), ctypes.c_int]
+        self.function = None
 
-            self.lib.calculateTotalInArray.restype = ctypes.c_int
+        self.compilation_error = None
 
-    def return_value_test(self, input_arr, expected_output):
-        result = self.lib.calculateTotalInArray(
-            (ctypes.c_int * len(input_arr))(*input_arr), len(input_arr))
+        if self.is_syntax_correct():
 
-        self.assertTrue(result == expected_output,
-                        f"Test case failed. Input: {input_arr}, Expected {expected_output}, but got {result}")
+            self.compilation_error = False
+
+            self.generate_shared_code()
+
+            self.lib = ctypes.CDLL(os.path.abspath("./test.so"))
+
+            if self.lib:
+                self.function = self.wrap_function(
+                    self.function_info['name'],
+                    self.function_info['return_type'],
+                    self.function_info['args_types'])
+
+        else:
+            self.compilation_error = True
+
+    def return_value_test(self, expected_output, *args):
+        message = "Correct !"
+
+        if self.compilation_error:
+            result = None
+            message = "Compilation error"
+            self.assert_(False, message)
+
+        else:
+            arg_values = []
+            arg_type_classes = self.function_info['args_types']
+
+            for i in range(len(arg_type_classes)):
+                arg_type = self.get_arg_type(arg_type_classes[i])
+                arg_values.append(self.get_arg_value(arg_type, args[i]))
+
+            result = self.function(*arg_values)
+
+            print(result)
+
+            if result != expected_output:
+                message = f"Test case failed. Input: {args}, Expected {expected_output}, but got {result}"
+
+            self.assertTrue(result == expected_output, message)
 
     # Add weight decorator to each test case
     for i, test_case in enumerate(test_cases):
         locals()[f'test_{i+1}'] = weight(test_case['weight'])(lambda self,
-                                                              test_case=test_case: self.return_value_test(test_case['input'], test_case['expected_output']))
+                                                              test_case=test_case: define_function_call(self.return_value_test, test_case))
--- src/utility.py
+++ src/utility.py
@@ -1,10 +1,17 @@
 import json
+import ctypes
 
 with open('config.json', 'r') as f:
     config = json.load(f)
 
 def get_code_file_name():
-    # print(config)
-    code_file = config['code_file']
-    # print("code_file", code_file)
-    return code_file
+    return config['code_file']
+
+def get_test_cases():
+    return config['test_cases']
+
+def get_function_info():
+    return config['function']
+
+def define_function_call(function, test_case):
+    return function(test_case['expected_output'], test_case['input'], len(test_case['input']))
